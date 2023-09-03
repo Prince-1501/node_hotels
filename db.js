49da1589415cec0976806853940482ea414327b6
Prince-1501 <princekumarrpcsvm10@gmail.com>
2023-09-03 12:35:23 +0530

    shift mongodb to online server

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 // Define the MongoDB connection URL
-const mongoURL = 'mongodb://localhost:27017/hotels' // Replace 'mydatabase' with your database name
+// const mongoURL = process.env.MONGODB_URL_LOCAL // Replace 'mydatabase' with your database name
+const mongoURL = process.env.MONGODB_URL;
 
 // Set up MongoDB connection
 mongoose.connect(mongoURL, {
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express();
 const db = require('./db');
+require('dotenv').config();
 
 const bodyParser = require('body-parser'); 
 app.use(bodyParser.json()); // req.body
-
+const PORT = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
     res.send('Welcome to our Hotel');
@@ -17,8 +18,7 @@ const menuItemRoutes = require('./routes/menuItemRoutes');
 // Use the routers
 app.use('/person', personRoutes);
 app.use('/menu', menuItemRoutes);
-
   
-app.listen(3000, ()=>{
+app.listen(PORT, ()=>{
     console.log('listening on port 3000');
 })
